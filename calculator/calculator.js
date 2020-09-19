519ae47f92088049fe008d4eae26697a62896e2d
Nidhish1407 <nidhishp07@gmail.com>
2020-09-20 00:40:08 +0530

    Solved calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,32 @@
-function add () {
-	
+function add (a,b) {
+	if(a==0 && b==0)return 0;
+	else return a+b;
 }
 
-function subtract () {
-	
+function subtract (a,b) {
+	return a-b;
 }
 
-function sum () {
-	
+function sum (a) {
+	let sum = 0;
+    a.forEach( i => sum+= +i);
+
+	return sum;
 }
 
-function multiply () {
-	
+function multiply (a) {
+	let tot = 1;
+	a.forEach( i => tot*= +i);
+	return tot;
 }
 
-function power() {
-	
+function power(a,b) {
+	return a**b;
 }
 
-function factorial() {
-	
+function factorial(a) {
+	if(a==1 || a==0)return 1;
+	else return a*factorial(a-1);
 }
 
 module.exports = {
