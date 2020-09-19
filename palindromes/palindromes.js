8fae0e2c4cbb7ccff32c1080277bfac2d1f5958f
Nidhish1407 <nidhishp07@gmail.com>
2020-09-20 01:42:00 +0530

    completed palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function() {
-
+const palindromes = function(s) {
+   let str = s.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{1,}/g,"").toLowerCase();
+              
+   let reverse= str.split("").reverse().join("");
+//    console.log(str);
+//    console.log(reverse);
+   return reverse==str;
 }
 
 module.exports = palindromes
