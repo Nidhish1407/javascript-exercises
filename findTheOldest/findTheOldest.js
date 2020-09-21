2ee39226c110fa6a084278a3150db7c596ac803a
Nidhish1407 <nidhishp07@gmail.com>
2020-09-21 21:57:41 +0530

    solved findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+   return people.reduce((oldest,person)=>{
+          oldestage  = (oldest.yearOfDeath?oldest.yearOfDeath:(new Date()).getFullYear()) - oldest.yearOfBirth;
+          currage = (person.yearOfDeath?person.yearOfDeath:(new Date()).getFullYear()) - person.yearOfBirth;
+          
+          return oldestage<currage?person:oldest;
+    })
 }
 
 module.exports = findTheOldest
