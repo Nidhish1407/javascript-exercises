f601fff55b04d8af837aedb1e69c4bef0839b967
Nidhish1407 <nidhishp07@gmail.com>
2020-09-21 21:13:20 +0530

    solved getTheTitles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+   let arr = [];
+   books.forEach((book)=>arr.push(book.title));
+   console.log(arr);
+   return arr;
 }
 
 module.exports = getTheTitles;
